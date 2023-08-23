d2e7185af3223fd374d262488497bfd4de93a1c5
Maria Gorodilova <mashunja13@yandex.ru>
2023-08-23 15:45:26 +0500

    бэкенд исправление ошибок

--- app.js
+++ app.js
@@ -24,5 +24,8 @@ app.use((req, res, next) => {
 });
 
 app.use(router);
+app.get('/', (req, res) => {
+  res.status(404).send('<h1>Страница не найдена</h1>');
+});
 
 app.listen(PORT, () => console.log(`Подключен: ${PORT}`));
--- controllers/cards.js
+++ controllers/cards.js
@@ -33,12 +33,15 @@ const deleteCard = (req, res) => {
   cardModal.findByIdAndRemove(req.params.cardId)
     .then((card) => {
       if (card === null) {
-        return res.status(NOT_FOUND).send({ message: 'Карточка с указанным _id не найдена.' });
+        return res.status(NOT_FOUND).send({ message: 'Карточка с указанным _id не найдена' });
       }
       return res.status(OK).send({ data: card });
     })
     .catch((error) => {
       console.log(error);
+      if (error.name === 'CastError') {
+        return res.status(NOT_FOUND).send({ message: 'Карточка с указанным _id не найдена' });
+      }
       return res.status(ERROR_CODE).send({ message: 'На сервере произошла ошибка' });
     });
 };
