36177b6263a84796e059b4b57a1ff9d0dee36302
Maria Gorodilova <mashunja13@yandex.ru>
2023-09-11 17:00:34 +0500

    Исправления после ревью3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,12 @@ const ErrorAuth = require('../errors/err_auth');
 const JWT_SECRET = 'some-secret-key';
 
 const auth = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new ErrorAuth('Передан неверный логин или пароль'));
+    return;
+  }
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
