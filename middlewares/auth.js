5385e7b1b8b38f033573b7caa30225752e69742c
Maria Gorodilova <mashunja13@yandex.ru>
2023-09-11 16:16:27 +0500

    Исправления после ревью3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,9 +9,10 @@ const auth = (req, res, next) => {
   if (!token) {
     next(new ErrorAuth('Ошибка'));
   }
+  const jwtToken = token.replace('***', '');
   let payload;
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(jwtToken, JWT_SECRET);
   } catch (error) {
     next(new ErrorAuth('Передан неверный логин или пароль'));
     return;
