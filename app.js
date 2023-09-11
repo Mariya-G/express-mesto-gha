fb388351db8b8b6854b11903b212eff4dc5a7243
Maria Gorodilova <mashunja13@yandex.ru>
2023-09-11 21:30:08 +0500

    Исправления после ревью5

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const cookieParser = require('cookie-parser');
+
 const { errors } = require('celebrate');
 const { validateAuth, validateInfoUser } = require('./middlewares/validators');
 
@@ -18,6 +20,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 mongoose.connect(DB_URL);
 
 app.use(express.json());
+app.use(cookieParser());
 app.post('/signup', validateInfoUser, createUser);
 app.post('/signin', validateAuth, login);
 
