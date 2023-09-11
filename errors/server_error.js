44e815039534cbe7a0d60eba0d78b2f3021f98e1
Maria Gorodilova <mashunja13@yandex.ru>
2023-09-11 15:45:00 +0500

    Исправления после ревью3

--- errors/server_error.js
+++ errors/server_error.js
@@ -1,6 +1,14 @@
 const ServerError = (err, req, res, next) => {
   console.error(`${err.message}: ошибка (${err.statusCode})`);
-  res.status(500).send({ message: 'На сервере произошла ошибка' });
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
   next();
 };
 
